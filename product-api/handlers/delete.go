1e93f300727ffd6fcfb7d2f8de0a818d87105f02
Uswatun Hasanah <uswatunhahaha@gmail.com>
2020-06-21 14:13:28 +0700

    add id checker at delete

--- product-api/handlers/delete.go
+++ product-api/handlers/delete.go
@@ -5,9 +5,7 @@ import (
 	"encoding/json"
 	"fmt"
 	"net/http"
-	"strconv"
 
-	"github.com/gorilla/mux"
 	"github.com/uswah-uswatunhahaha/building-microservices/product-api/data"
 )
 
@@ -16,11 +14,7 @@ func (p *Products) Delete(rw http.ResponseWriter, r *http.Request) {
 	p.l.Println("[DELETE] delete records")
 	rw.Header().Add("Content-Type", "application/json")
 
-	vars := mux.Vars(r)
-	id, err := strconv.Atoi(vars["id"])
-	if err != nil {
-		http.Error(rw, "Unable to convert id", http.StatusBadRequest)
-	}
+	id := getProductID(r)
 
 	ctx, cancel := context.WithCancel(context.Background())
 
@@ -28,13 +22,14 @@ func (p *Products) Delete(rw http.ResponseWriter, r *http.Request) {
 
 	var prod data.Product
 
-	err = json.NewDecoder(r.Body).Decode(&prod)
+	err := json.NewDecoder(r.Body).Decode(&prod)
 	if err != nil {
 		p.l.Println(err)
 	}
 
 	err = p.hapus(ctx, prod, id)
 	if err != nil {
+		data.ToJSON(&GenericError{Message: err.Error()}, rw)
 		p.l.Println(err)
 	}
 
@@ -45,6 +40,12 @@ func (p *Products) hapus(ctx context.Context, prod data.Product, id int) error {
 	queryText := fmt.Sprintf("DELETE FROM tbl_product where id = %d", id)
 
 	fmt.Println(queryText)
+
+	// Check ID existance before exec update
+	isIDExist := p.findProductID(id)
+	if isIDExist == 0 {
+		return ErrProductNotFound
+	}
 
 	_, err := p.database.ExecContext(ctx, queryText)
 
