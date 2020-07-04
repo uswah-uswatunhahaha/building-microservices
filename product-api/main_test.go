d984d5327cab63f724266b1ae962b16bc4fd5c25
Uswatun Hasanah <uswatunhahaha@gmail.com>
2020-07-04 17:39:53 +0700

    test client

--- product-api/main_test.go
+++ product-api/main_test.go
@@ -1,6 +1,24 @@
 package main
 
-import "testing"
+import (
+	"fmt"
+	"testing"
+
+	"github.com/uswah-uswatunhahaha/building-microservices/product-api/sdk/client"
+	"github.com/uswah-uswatunhahaha/building-microservices/product-api/sdk/client/product"
+)
 
 func TestOurClient(t *testing.T) {
+	cfg := client.DefaultTransportConfig().WithHost("localhost:9090")
+	c := client.NewHTTPClientWithConfig(nil, cfg)
+
+	params := product.NewListProductsParams()
+	prod, err := c.Product.ListProducts(params)
+
+	if err != nil {
+		t.Fatal(err)
+	}
+	fmt.Printf("%#v", prod.GetPayload()[0])
+	t.Fail()
+
 }
